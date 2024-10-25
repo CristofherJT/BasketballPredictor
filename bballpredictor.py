60e45e92ff17ce3053a3e060308e17916d323a38
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2024-10-25 17:04:53 -0500

    Updated bballpredictor.py
    Removed commonallplayers library and tested input

--- bballpredictor.py
+++ bballpredictor.py
@@ -7,11 +7,9 @@ import numpy as np
 import os
 #os.system(f"{os.sys.executable} -m pip install nba_api")
 
-from nba_api.stats.endpoints import playergamelog, commonallplayers
+from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 
-all_players = commonallplayers.CommonAllPlayers()
-players_df = all_players.get_data_frames()[0]
 
 #Function will get player data over multiple seasons
 def get_player_data(player_id, seasons):
@@ -21,14 +19,19 @@ def get_player_data(player_id, seasons):
         game_logs = playergamelog.PlayerGameLog(player_id = player_id, season = season)
         game_data_df = game_logs.get_data_frames()[0]
         #Drops unnecessary data
-        game_data_df.drop(['Game_ID','VIDEO_AVAILABLE'], axis = 1, inplace=True)
+        game_data_df.drop(['Player_ID','GAME_DATE','Game_ID','VIDEO_AVAILABLE'], axis = 1, inplace=True)
         all_data.append(game_data_df)
 
     combine_data = pd.concat(all_data, ignore_index = True)
 
     return combine_data
 
-playerID = '2544'
+player_list = players.get_active_players()
+
+playerIn = input('Name a current NBA player: ')
+
+player = [player for player in player_list if player['full_name'] == playerIn][0]
+playerID = player['id']
 seasons = ['2021-22', '2022-23', '2023-24']
 
 print(get_player_data(playerID, seasons))
