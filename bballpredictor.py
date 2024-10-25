2cb93052b6114d50c45bda93d36f9ac68a312cdd
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2024-10-24 23:25:07 -0500

    Update bballpredictor.py
    Added a line to automatically install nba_api to device

--- bballpredictor.py
+++ bballpredictor.py
@@ -3,6 +3,10 @@ import pandas as pd
 #Lets us create and work with arrays of data
 import numpy as np
 
+#Will download nba_api to device
+import os
+os.system(f"{os.sys.executable} -m pip install nba_api")
+
 import nba_api 
 
 from nba_api.stats.endpoints import playergamelog
