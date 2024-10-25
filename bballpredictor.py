8d5dd495ad6f4b63ae1f127f9ec2fbb92b2233f5
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2024-10-25 14:14:24 -0500

    Update bballpredictor.py
    Omitted the auto nba_api install line and checking data frames

--- bballpredictor.py
+++ bballpredictor.py
@@ -5,10 +5,14 @@ import numpy as np
 
 #Will download nba_api to device
 import os
-os.system(f"{os.sys.executable} -m pip install nba_api")
+#os.system(f"{os.sys.executable} -m pip install nba_api")
 
-import nba_api 
-
-from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.endpoints import playergamelog, commonallplayers
 from nba_api.stats.static import players
 
+all_players = commonallplayers.CommonAllPlayers()
+players_df = all_players.get_data_frames()[0]
+
+
+
+print(players_df)
