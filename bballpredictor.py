ef8acfcf98b57f442612c4c71d624ee7b8d86908
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2024-10-31 18:38:59 -0500

    Update bballpredictor.py
    Made a function to gather team defensive data throughout certain seasons

--- bballpredictor.py
+++ bballpredictor.py
@@ -5,8 +5,8 @@ import pandas as pd
 import os
 #os.system(f"{os.sys.executable} -m pip install nba_api")
 
-from nba_api.stats.endpoints import playergamelog
-from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog, leaguedashteamstats, teamdashptshots
+from nba_api.stats.static import players, teams
 
 
 #Function will get player data over multiple seasons
@@ -14,24 +14,38 @@ def get_player_data(player_id, seasons):
     all_data = []
 
     for season in seasons:
-        game_logs = playergamelog.PlayerGameLog(player_id = player_id, season = season)
-        game_data_df = game_logs.get_data_frames()[0]
+        game_logs = playergamelog.PlayerGameLog(player_id = player_id, season = season).get_data_frames()[0]
         #Drops unnecessary data
-        game_data_df.drop(['Player_ID','Game_ID','GAME_DATE','VIDEO_AVAILABLE'], axis = 1, inplace=True)
-        all_data.append(game_data_df)
+        game_logs.drop(['Player_ID','Game_ID','GAME_DATE','VIDEO_AVAILABLE'], axis = 1, inplace=True)
+        all_data.append(game_logs)
 
     combine_data = pd.concat(all_data, ignore_index = True)
 
     return combine_data
 
+def get_team_defensive_data(seasons):
+    team_defense_data = []
+
+    for season in seasons:
+        defense_logs = leaguedashteamstats.LeagueDashTeamStats(team_id = team_id, season = season, measure_type_detailed_defense = 'Defense').get_data_frames()[0]
+        team_defense_data.append(defense_logs)
+
+    combined_defense_data = pd.concat(team_defense_data, ignore_index = True)
+
+    return combined_defense_data
+
+
 seasons = ['2021-22', '2022-23', '2023-24', '2024-25']
 player_list = players.get_active_players()
 
-playerIn = input('Name a current NBA player: ')
+playerIn = input('Name a current NBA player by their full name: ')
+teamIn = input('What team is the player playing against?: ')
 
 #Will look through active roster in search of the input
 player = [player for player in player_list if player['full_name'] == playerIn][0]
 playerID = player['id']
 
+team = teams.find_teams_by_full_name('teamIn')
+print(team)
 
-print(get_player_data(playerID, seasons))
+print(get_team_defensive_data(seasons))
